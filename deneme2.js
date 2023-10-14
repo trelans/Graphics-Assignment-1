e7c4ef4fd14c65dfc159e8518c77f56c07cbc5bc
trelans <59960883+trelans@users.noreply.github.com>
2023-10-14 18:59:26 +0300

    new features

--- deneme2.js
+++ deneme2.js
@@ -24,10 +24,10 @@ var colors = [
 
 
 
-const numRows = 10; // Number of rows
-const numColumns = 20; // Number of columns
+const numRows = 40; // Number of rows
+const numColumns = 40; // Number of columns
 
-const cubeSize = 0.1; // Size of each cube
+const cubeSize = 0.05; // Size of each cube
 
 // Define vertices and colors for the triangles
 const vertices = [];
@@ -41,6 +41,15 @@ const undoHistory = {
 };
 
 
+let isDragging = false;
+let lastX = 0;
+let lastY = 0;
+
+let zoomScale = 1;
+let xOffset = 0;
+let yOffset = 0;
+
+const ZOOM_FACTOR = 0.1; // The zoom sensitivity. Adjust as needed.
 
 window.onload = function init() {
     canvas = document.getElementById("gl-canvas");
@@ -57,21 +66,32 @@ window.onload = function init() {
     //  Configure WebGL
     //
     gl.viewport(0, 0, canvas.width, canvas.height);
+
     gl.clearColor(1.0, 1.0, 1.0, 1.0);
-                 // Set size value
+    // Set size value
     const size = gl.getUniformLocation(program, 'size');
     gl.uniform1f(size, 0.1); // Cube Size
-
-     for (let m = 0; m < numRows* numColumns ; m++) {
-            for (let l = 0; l < 4; l++) {
-              for (let k = 0; k < 3; k++) {
-                const color =  vec4(0.0, 1.0, 1.0, 1.0);
+    //manageZoomAndPan(canvas);
+    // Orthographic Projection
+    const left = 0;
+    const right = 2;
+    const bottom = 0;
+    const top = 2;
+    const near = -1;
+    const far = 1;
+    const projectionMatrix = ortho(left, right, bottom, top, near, far);
+    const u_ProjectionMatrix = gl.getUniformLocation(program, 'projectionMatrix');
+    gl.uniformMatrix4fv(u_ProjectionMatrix, false, flatten(projectionMatrix));
+  for (let m = 0; m < numRows * numColumns; m++) {
+        for (let l = 0; l < 4; l++) {
+            for (let k = 0; k < 3; k++) {
+                const color = vec4(0.0, 1.0, 1.0, 1.0);
                 vertexColors.push(color);
             }
-                index++;  
-            }
+            index++;
         }
-          
+    }
+
 
     for (let i = 0; i < numRows; i++) {
         for (let j = 0; j < numColumns; j++) {
@@ -81,30 +101,30 @@ window.onload = function init() {
 
             // Define the vertices for a single square divided into four triangles
             const squareVertices = [
-        
+
                 x, y + cubeSize, z,
-                x + cubeSize/2, y + cubeSize/2, z, //left
-                x , y, z,
+                x + cubeSize / 2, y + cubeSize / 2, z, //left
+                x, y, z,
 
                 x + cubeSize, y + cubeSize, z,
                 x, y + cubeSize, z,
-                x + cubeSize/2, y + cubeSize/2, z, //top
+                x + cubeSize / 2, y + cubeSize / 2, z, //top
 
                 x + cubeSize, y + cubeSize, z,
-                x + cubeSize/2, y + cubeSize/2, z, //right
+                x + cubeSize / 2, y + cubeSize / 2, z, //right
                 x + cubeSize, y, z,
 
-                x, y , z,
-                x + cubeSize/2, y + cubeSize/2, z, //bottom
-                x + cubeSize, y, z              
+                x, y, z,
+                x + cubeSize / 2, y + cubeSize / 2, z, //bottom
+                x + cubeSize, y, z
             ];
 
-// Define a single color for each triangle
-         
+            // Define a single color for each triangle
+
 
 
             vertices.push(...squareVertices);
-                  
+
         }
     }
     pushState(); // Save the initial state
@@ -125,20 +145,20 @@ window.onload = function init() {
     gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vColor);
 
- 
+
     gl.drawArrays(gl.TRIANGLES, 0, vertices.length / 3);
 
 
 
-       // Add a mousedown event listener
-    canvas.addEventListener("mousedown", function(event) {
+    // Add a mousedown event listener
+    canvas.addEventListener("mousedown", function (event) {
         isMouseDown = true;
         // Start the recursive function
         drawing();
     });
 
     // Add a mousemove event listener to enable painting while dragging
-    canvas.addEventListener("mousemove", function(event) {
+    canvas.addEventListener("mousemove", function (event) {
         if (isMouseDown) {
             // Set the flag to indicate painting
             isPainting = true;
@@ -147,15 +167,15 @@ window.onload = function init() {
     });
 
     // Add a mouseup event listener to stop painting when the mouse is released
-    canvas.addEventListener("mouseup", function(event) {
+    canvas.addEventListener("mouseup", function (event) {
         isMouseDown = false;
         isPainting = false;
         pushState();
     });
 
-    canvas.addEventListener("mouseout", function(event) {
-      isMouseDown = false;
-       isPainting = false;
+    canvas.addEventListener("mouseout", function (event) {
+        isMouseDown = false;
+        isPainting = false;
     });
 
     const undoButton = document.getElementById("undo-button");
@@ -166,17 +186,66 @@ window.onload = function init() {
 
     const loadButton = document.getElementById("load-button");
     loadButton.addEventListener("click", loadData);
-   }
+}
 
 
 
 function render() {
-    
-    gl.clear( gl.COLOR_BUFFER_BIT );
-    gl.drawArrays( gl.TRIANGLES, 0, vertices.length / 3 );
+
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.TRIANGLES, 0, vertices.length / 3);
 
     window.requestAnimFrame(render);
 
+}
+
+function manageZoomAndPan(canvas) {
+    canvas.onmousedown = function(event) {
+        isDragging = true;
+        lastX = event.clientX;
+        lastY = event.clientY;
+    }
+
+    canvas.onmouseup = function(event) {
+        isDragging = false;
+    }
+
+    canvas.onmousemove = function(event) {
+        if (isDragging) {
+            let dx = event.clientX - lastX;
+            let dy = event.clientY - lastY;
+
+            // Adjust the offsets based on the mouse drag
+            xOffset -= dx / canvas.width * zoomScale;
+            yOffset += dy / canvas.height * zoomScale;
+
+            updateProjection();
+
+            lastX = event.clientX;
+            lastY = event.clientY;
+        }
+    }
+
+    canvas.onwheel = function(event) {
+        // Adjust the zoom scale based on the mouse wheel movement
+        zoomScale *= (event.deltaY > 0) ? 1 + ZOOM_FACTOR : 1 - ZOOM_FACTOR;
+
+        updateProjection();
+    }
+}
+
+function updateProjection() {
+    const left = -zoomScale + xOffset;
+    const right = zoomScale + xOffset;
+    const bottom = -zoomScale + yOffset;
+    const top = zoomScale + yOffset;
+
+    const projectionMatrix = ortho(left, right, bottom, top, -1, 1);
+    const u_ProjectionMatrix = gl.getUniformLocation(program, 'projectionMatrix');
+    gl.uniformMatrix4fv(u_ProjectionMatrix, false, flatten(projectionMatrix));
+    
+    // Render function to redraw your scene with the updated projection
+    render();
 }
 
 function drawing() {
@@ -184,92 +253,92 @@ function drawing() {
 
     if (isMouseDown) {
 
-            // Get the mouse coordinates relative to the canvas
-            var x = event.clientX - canvas.getBoundingClientRect().left;
-            var y = event.clientY - canvas.getBoundingClientRect().top;
+        // Get the mouse coordinates relative to the canvas
+        var x = event.clientX - canvas.getBoundingClientRect().left;
+        var y = event.clientY - canvas.getBoundingClientRect().top;
 
-            // Convert mouse coordinates to WebGL coordinates (-1 to 1)
-            x = (2 * x / canvas.width) - 1;
-            y = 1 - (2 * y / canvas.height);
-
-           
-            console.log(x , y);
-
-            var leftOrder = Math.floor(x/cubeSize + 1);
-            var bottomOrder = Math.floor(y/cubeSize + 1);
-
-            var maxY = bottomOrder * cubeSize;
-            var middleY = maxY - (cubeSize/2);
-            var minY = (bottomOrder-1) * cubeSize ;
-
-            var maxX = leftOrder * cubeSize;
-            var middleX = maxX - (cubeSize/2);
-            var minX = (leftOrder-1) * cubeSize ;
-
-            // top
-            var G1x = (minX + middleX + maxX) / 3; 
-            var G1y = (middleY + maxY + maxY) / 3;
+        // Convert mouse coordinates to WebGL coordinates (-1 to 1)
+        x = (2 * x / canvas.width) ;
+        y = 2 - (2 * y / canvas.height);
 
 
-            // left
-            var G2x = (minX + minX + middleX) / 3; 
-            var G2y = (minY + maxY + middleY) / 3;
+        console.log(x, y);
+
+        var leftOrder = Math.floor(x / cubeSize + 1);
+        var bottomOrder = Math.floor(y / cubeSize + 1);
+
+        var maxY = bottomOrder * cubeSize;
+        var middleY = maxY - (cubeSize / 2);
+        var minY = (bottomOrder - 1) * cubeSize;
+
+        var maxX = leftOrder * cubeSize;
+        var middleX = maxX - (cubeSize / 2);
+        var minX = (leftOrder - 1) * cubeSize;
+
+        // top
+        var G1x = (minX + middleX + maxX) / 3;
+        var G1y = (middleY + maxY + maxY) / 3;
 
 
-            // right
-            var G3x = (maxX + middleX + maxX) / 3; 
-            var G3y = (minY + maxY + middleY) / 3;
+        // left
+        var G2x = (minX + minX + middleX) / 3;
+        var G2y = (minY + maxY + middleY) / 3;
 
 
-            // down
-            var G4x = (minX + middleX + maxX) / 3; 
-            var G4y = (middleY + minY + minY) / 3;
+        // right
+        var G3x = (maxX + middleX + maxX) / 3;
+        var G3y = (minY + maxY + middleY) / 3;
 
 
-                // Calculate distances to each of the G points
-            var distanceToG1 = Math.sqrt(Math.pow(G1x - x, 2) + Math.pow(G1y - y, 2));
-            var distanceToG2 = Math.sqrt(Math.pow(G2x - x, 2) + Math.pow(G2y - y, 2));
-            var distanceToG3 = Math.sqrt(Math.pow(G3x - x, 2) + Math.pow(G3y - y, 2));
-            var distanceToG4 = Math.sqrt(Math.pow(G4x - x, 2) + Math.pow(G4y - y, 2));
-
-            // Find the closest point
-            var offset;
-            var minDistance = Math.min(distanceToG1, distanceToG2, distanceToG3, distanceToG4);
+        // down
+        var G4x = (minX + middleX + maxX) / 3;
+        var G4y = (middleY + minY + minY) / 3;
 
 
-            if (minDistance === distanceToG1) {
-                offset = 1;
-            } else if (minDistance === distanceToG2) {
-                offset = 0;
-            } else if (minDistance === distanceToG3) {
-                offset = 2;
-            } else {
-                offset = 3;
-            }
-            console.log( "Soldan: " + leftOrder ); 
-            console.log( "Aşağıdan: " + bottomOrder ); 
+        // Calculate distances to each of the G points
+        var distanceToG1 = Math.sqrt(Math.pow(G1x - x, 2) + Math.pow(G1y - y, 2));
+        var distanceToG2 = Math.sqrt(Math.pow(G2x - x, 2) + Math.pow(G2y - y, 2));
+        var distanceToG3 = Math.sqrt(Math.pow(G3x - x, 2) + Math.pow(G3y - y, 2));
+        var distanceToG4 = Math.sqrt(Math.pow(G4x - x, 2) + Math.pow(G4y - y, 2));
+
+        // Find the closest point
+        var offset;
+        var minDistance = Math.min(distanceToG1, distanceToG2, distanceToG3, distanceToG4);
 
 
-            console.log( "maxY: " + maxY ); 
-            console.log( "middleY: " + middleY ); 
+        if (minDistance === distanceToG1) {
+            offset = 1;
+        } else if (minDistance === distanceToG2) {
+            offset = 0;
+        } else if (minDistance === distanceToG3) {
+            offset = 2;
+        } else {
+            offset = 3;
+        }
+        console.log("Soldan: " + leftOrder);
+        console.log("Aşağıdan: " + bottomOrder);
 
-            console.log( "maxX: " + maxX ); 
-            console.log( "middleX: " + middleX ); 
-          
-            console.log( "Closest: " + offset ); 
 
-            console.log(vertexColors);
-             // Change Color
+        console.log("maxY: " + maxY);
+        console.log("middleY: " + middleY);
 
-            var indexColor = 12*(((bottomOrder-1) * numColumns + leftOrder)-1) + (3 * offset);
+        console.log("maxX: " + maxX);
+        console.log("middleX: " + middleX);
 
-            for (var i = indexColor; i < indexColor + 3; i++) {
-                vertexColors[i] =  currentColor;
-                  console.log(i);
-            }
-            console.log(vertexColors)
-          
-          // Bind the buffer again (in case it's not already bound)
+        console.log("Closest: " + offset);
+
+        console.log(vertexColors);
+        // Change Color
+
+        var indexColor = 12 * (((bottomOrder - 1) * numColumns + leftOrder) - 1) + (3 * offset);
+
+        for (var i = indexColor; i < indexColor + 3; i++) {
+            vertexColors[i] = currentColor;
+            console.log(i);
+        }
+        console.log(vertexColors)
+
+        // Bind the buffer again (in case it's not already bound)
         gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
 
         // Update the buffer data with the modified vertexColors array
@@ -281,33 +350,33 @@ function drawing() {
         gl.enableVertexAttribArray(vColor);
 
         render();
-     
 
-    } 
+
+    }
 }
 
 
-    function changeColor(color) {
-        var colorMap = {
-            'black': [0.0, 0.0, 0.0, 1.0],
-            'red': [1.0, 0.0, 0.0, 1.0],
-            'green': [0.0, 1.0, 0.0, 1.0],
-            'blue': [0.0, 0.0, 1.0, 1.0],  
-            'ERASER': [0.0, 1.0, 1.0, 1.0],
-            
-        };
+function changeColor(color) {
+    var colorMap = {
+        'black': [0.0, 0.0, 0.0, 1.0],
+        'red': [1.0, 0.0, 0.0, 1.0],
+        'green': [0.0, 1.0, 0.0, 1.0],
+        'blue': [0.0, 0.0, 1.0, 1.0],
+        'ERASER': [0.0, 1.0, 1.0, 1.0],
 
-        if (colorMap[color]) {
-            currentColor = colorMap[color];
-            // You can pass the new color to your WebGL rendering function here
-            // For example, you can update the uniform variable for the color in your shader.
-            // Example:
-            // gl.uniform4fv(gl.getUniformLocation(program, "uColor"), currentColor);
-        }
+    };
+
+    if (colorMap[color]) {
+        currentColor = colorMap[color];
+        // You can pass the new color to your WebGL rendering function here
+        // For example, you can update the uniform variable for the color in your shader.
+        // Example:
+        // gl.uniform4fv(gl.getUniformLocation(program, "uColor"), currentColor);
     }
+}
 
 
-    // Function to save the current state to the undo history
+// Function to save the current state to the undo history
 function pushState() {
     if (undoHistory.currentIndex < undoHistory.vertexStates.length - 1) {
         // Remove redo history when a new action is performed
@@ -390,7 +459,7 @@ function loadFileData(inputFile) {
     if (file) {
         const reader = new FileReader();
 
-        reader.onload = function(event) {
+        reader.onload = function (event) {
             const jsonSaveData = event.target.result;
             const saveData = JSON.parse(jsonSaveData);
 
@@ -413,4 +482,3 @@ function loadFileData(inputFile) {
         reader.readAsText(file);
     }
 }
-
