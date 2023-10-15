35d9606b199fb57e75e3394fef1851818050d53d
trelans <59960883+trelans@users.noreply.github.com>
2023-10-15 16:03:14 +0300

    sdsad
    sdsad

--- deneme2.js
+++ deneme2.js
@@ -9,6 +9,7 @@ var VPosition;
 var vColor;
 var isMouseDown = false;
 var isPainting = false;
+var isErasing = false;
 var program;
 var currentColor = vec4(0.0, 0.0, 0.0, 1.0); // Initial color (black)
 
@@ -31,14 +32,21 @@ const cubeSize = 0.05; // Size of each cube
 
 // Define vertices and colors for the triangles
 const vertices = [];
+let currentVertices = [];
+let currentColors = [];
 const vertexColors = [];
+let dynamicVertices = [];
+let dynamicColors = [];
 
+var indexColor;
+var previousIndexColor = -1;
 const undoHistory = {
     vertexStates: [],
     colorStates: [],
     currentIndex: -1,
     maxHistoryLength: 30
 };
+
 
 
 let isDragging = false;
@@ -82,13 +90,15 @@ window.onload = function init() {
     const projectionMatrix = ortho(left, right, bottom, top, near, far);
     const u_ProjectionMatrix = gl.getUniformLocation(program, 'projectionMatrix');
     gl.uniformMatrix4fv(u_ProjectionMatrix, false, flatten(projectionMatrix));
+
+// will delete
   for (let m = 0; m < numRows * numColumns; m++) {
         for (let l = 0; l < 4; l++) {
             for (let k = 0; k < 3; k++) {
                 const color = vec4(0.0, 1.0, 1.0, 1.0);
                 vertexColors.push(color);
             }
-            index++;
+        
         }
     }
 
@@ -130,7 +140,7 @@ window.onload = function init() {
     pushState(); // Save the initial state
     VBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, VBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, numColumns * numRows * 3 * 16, gl.STATIC_DRAW);
 
     VPosition = gl.getAttribLocation(program, "position");
     gl.vertexAttribPointer(VPosition, 3, gl.FLOAT, false, 0, 0);
@@ -139,14 +149,11 @@ window.onload = function init() {
 
     cBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertexColors), gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, numColumns * numRows * 4 *32, gl.STATIC_DRAW);
 
     vColor = gl.getAttribLocation(program, "vColor");
     gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vColor);
-
-
-    gl.drawArrays(gl.TRIANGLES, 0, vertices.length / 3);
 
 
 
@@ -188,15 +195,56 @@ window.onload = function init() {
     loadButton.addEventListener("click", loadData);
 }
 
+function reallocateBuffers() {
 
 
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, VBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, numColumns * numRows * 3 * 16, gl.STATIC_DRAW);
+
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, numColumns * numRows * 4 *32, gl.STATIC_DRAW);
+
+
+}
+
+function fillBuffers() {
+
+    reallocateBuffers();
+    for (var i = 0; i < currentVertices.length; i++) {
+       
+         var offsetVertex = i*12;
+         var offsetColor = i*16;
+         gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+         gl.bufferSubData(gl.ARRAY_BUFFER, offsetColor, flatten(currentColors[i]));
+
+         gl.bindBuffer(gl.ARRAY_BUFFER, VBuffer);
+         gl.bufferSubData(gl.ARRAY_BUFFER, offsetVertex , flatten(currentVertices[i]));
+    }
+    
+
+}
 function render() {
 
     gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.drawArrays(gl.TRIANGLES, 0, vertices.length / 3);
+    gl.drawArrays(gl.TRIANGLES, 0, index);
 
     window.requestAnimFrame(render);
 
+}
+
+function updateBuffers() {
+  
+    // Updating vertex color buffer
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(currentColors), gl.STATIC_DRAW);
+
+    // Updating vertex position buffer
+    gl.bindBuffer(gl.ARRAY_BUFFER, VBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(currentVertices), gl.STATIC_DRAW);
+
+    render();
 }
 
 function manageZoomAndPan(canvas) {
@@ -262,7 +310,7 @@ function drawing() {
         y = 2 - (2 * y / canvas.height);
 
 
-        console.log(x, y);
+        
 
         var leftOrder = Math.floor(x / cubeSize + 1);
         var bottomOrder = Math.floor(y / cubeSize + 1);
@@ -315,46 +363,106 @@ function drawing() {
         } else {
             offset = 3;
         }
-        console.log("Soldan: " + leftOrder);
-        console.log("Aşağıdan: " + bottomOrder);
 
-
-        console.log("maxY: " + maxY);
-        console.log("middleY: " + middleY);
-
-        console.log("maxX: " + maxX);
-        console.log("middleX: " + middleX);
-
-        console.log("Closest: " + offset);
-
-        console.log(vertexColors);
         // Change Color
 
-        var indexColor = 12 * (((bottomOrder - 1) * numColumns + leftOrder) - 1) + (3 * offset);
+        indexColor = 12 * (((bottomOrder - 1) * numColumns + leftOrder) - 1) + (3 * offset);
+    
+        if ((indexColor != previousIndexColor) || isErasing) {
+          
+                var offsetVertex = index*12;
+                var offsetColor = index*16;
 
-        for (var i = indexColor; i < indexColor + 3; i++) {
-            vertexColors[i] = currentColor;
-            console.log(i);
+                 previousIndexColor = indexColor;
+
+                 for (var i = indexColor; i < indexColor + 3; i++) {
+                    
+                 
+                    if (!isErasing) {
+
+                          index++;
+                           console.log("ındex" + index) 
+                          currentVertices.push([vertices[i*3] , vertices[(i*3) + 1], vertices[(i*3) + 2] ]);
+                               currentColors.push(currentColor);
+                    }
+                    dynamicVertices.push( [vertices[i*3] , vertices[(i*3) + 1], vertices[(i*3) + 2] ]);
+                    dynamicColors.push(currentColor);
+               
+                    
+                  
+                }
+                 console.log("V")
+               console.log(currentVertices);
+                   console.log("C")
+               console.log(currentColors);
+                 if (isErasing) {
+
+                    var found = false
+                    var foundIndex = -1;
+                    for (var i = 0; i < currentVertices.length; i = i +3) {
+                        var currentVertex = currentVertices[i];
+                        var currentVertex2 = currentVertices[i+1];
+                        var currentVertex3 = currentVertices[i+2];
+                        if (arraysAreEqual(currentVertex, dynamicVertices[0]) && arraysAreEqual(currentVertex2, dynamicVertices[1]) && arraysAreEqual(currentVertex3, dynamicVertices[2])) {
+                            found = true;
+                            foundIndex = i;
+                            break;
+                        }
+                    }
+
+                    if (found) {
+                           console.log("V")
+                           console.log(currentVertices);
+                            console.log("C")
+                           console.log(currentColors);
+                           currentVertices.splice(foundIndex, 3);
+                           currentColors.splice(foundIndex, 3);
+                           console.log(currentVertices);
+                           console.log(currentColors);  
+                           fillBuffers();
+                           index = index-3;
+                           console.log("ındex" + index)
+                           render();
+
+                    } else {
+                        console.log("dynamicVertices[0] not found in currentVertices");
+                    }
+         
+
+                 }
+                 else {
+                           // Updating vertex color buffer
+                        gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+                        gl.bufferSubData(gl.ARRAY_BUFFER, offsetColor, flatten(dynamicColors));
+        
+
+                        // Assuming vBuffer is your buffer for vertices
+                        gl.bindBuffer(gl.ARRAY_BUFFER, VBuffer);
+                        gl.bufferSubData(gl.ARRAY_BUFFER, offsetVertex , flatten(dynamicVertices));
+                        render();
+                 }
+               
+         
+                dynamicVertices = [];
+                dynamicColors = [];
+             
+
         }
-        console.log(vertexColors)
-
-        // Bind the buffer again (in case it's not already bound)
-        gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
-
-        // Update the buffer data with the modified vertexColors array
-        gl.bufferData(gl.ARRAY_BUFFER, flatten(vertexColors), gl.STATIC_DRAW);
-
-        // Bind the vColor attribute again (if it's not already bound)
-        var vColor = gl.getAttribLocation(program, "vColor");
-        gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
-        gl.enableVertexAttribArray(vColor);
-
-        render();
-
-
     }
 }
 
+
+function arraysAreEqual(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    for (var j = 0; j < arr1.length; j++) {
+        if (arr1[j] !== arr2[j]) {
+            return false;
+        }
+    }
+    return true;
+}
 
 function changeColor(color) {
     var colorMap = {
@@ -362,8 +470,6 @@ function changeColor(color) {
         'red': [1.0, 0.0, 0.0, 1.0],
         'green': [0.0, 1.0, 0.0, 1.0],
         'blue': [0.0, 0.0, 1.0, 1.0],
-        'ERASER': [0.0, 1.0, 1.0, 1.0],
-
     };
 
     if (colorMap[color]) {
@@ -376,6 +482,26 @@ function changeColor(color) {
 }
 
 
+function redo() {
+    if (undoHistory.currentIndex < undoHistory.vertexStates.length - 1) {
+        undoHistory.currentIndex++;
+
+        // Restore the next state from the undo history
+        const nextVertices = undoHistory.vertexStates[undoHistory.currentIndex];
+        const nextColors = undoHistory.colorStates[undoHistory.currentIndex];
+
+        // Update the current state
+        currentVertices.length = 0;
+        currentVertices.push(...nextVertices);
+        currentColors.length = 0;
+        currentColors.push(...nextColors);
+
+        fillBuffers();
+    }
+}
+
+
+
 // Function to save the current state to the undo history
 function pushState() {
     if (undoHistory.currentIndex < undoHistory.vertexStates.length - 1) {
@@ -384,8 +510,8 @@ function pushState() {
         undoHistory.colorStates.splice(undoHistory.currentIndex + 1);
     }
 
-    const clonedVertices = vertices.slice();
-    const clonedColors = vertexColors.slice();
+    const clonedVertices = currentVertices.slice();
+    const clonedColors = currentColors.slice();
 
     undoHistory.vertexStates.push(clonedVertices);
     undoHistory.colorStates.push(clonedColors);
@@ -409,26 +535,27 @@ function undo() {
         const previousColors = undoHistory.colorStates[undoHistory.currentIndex];
 
         // Update the current state
-        vertices.length = 0;
-        vertices.push(...previousVertices);
-        vertexColors.length = 0;
-        vertexColors.push(...previousColors);
-
-        // Update WebGL buffers with the previous state
-        gl.bindBuffer(gl.ARRAY_BUFFER, VBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-
-        gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, flatten(vertexColors), gl.STATIC_DRAW);
-
-        render(); // Redraw the canvas
+        currentVertices.length = 0;
+        currentVertices.push(...previousVertices);
+        currentColors.length = 0;
+        currentColors.push(...previousColors);
+ 
+      fillBuffers();
     }
 }
 
+function eraser() {
+    isErasing = true;
+
+}
+function brush() {
+    isErasing = false;
+    
+}
 function saveDataToFile() {
     const saveData = {
-        vertices: vertices,
-        vertexColors: vertexColors,
+        vertices: currentVertices,
+        vertexColors: currentColors,
     };
 
     const jsonSaveData = JSON.stringify(saveData);
@@ -471,14 +598,35 @@ function loadFileData(inputFile) {
 
             // Update WebGL buffers with the loaded data
             gl.bindBuffer(gl.ARRAY_BUFFER, VBuffer);
-            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(currentVertices), gl.STATIC_DRAW);
 
             gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
-            gl.bufferData(gl.ARRAY_BUFFER, flatten(vertexColors), gl.STATIC_DRAW);
+            gl.bufferData(gl.ARRAY_BUFFER, flatten(currentColors), gl.STATIC_DRAW);
 
             render(); // Redraw the canvas with the loaded data
         };
 
         reader.readAsText(file);
     }
+}
+
+function identifySelectedVertices() {
+    selectedVertices = [];
+    // Loop through your vertices and check if they fall within the selection rectangle.
+    var minX = Math.min(selectionStart.x, selectionEnd.x);
+    var maxX = Math.max(selectionStart.x, selectionEnd.x);
+    var minY = Math.min(selectionStart.y, selectionEnd.y);
+    var maxY = Math.max(selectionStart.y, selectionEnd.y);
+
+    for (var i = 0; i < vertices.length; i += 3) {
+        var x = vertices[i];
+        var y = vertices[i + 1];
+
+        if (x >= minX && x <= maxX && y >= minY && y <= maxY) {
+            selectedVertices.push(i / 3); // Store the index of the selected vertex.
+        }
+    }
+
+    // Now you have the indices of the selected vertices in the selectedVertices array.
+    // You can use this information to move the selected triangles as needed.
 }
